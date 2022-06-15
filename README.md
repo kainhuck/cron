9c8d64fc9f72257645ef7fec76489745304ec386
kainhuck <1628321264@qq.com>
2022-06-15 15:56:17 +0800

    add .

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # cron
 
-## Based on github.com/robfig/cron/v3, but more convenient
+*Based on github.com/robfig/cron/v3, but more convenient*
+
+## Usaga
+
+```go
+
+package main
+
+import (
+    "fmt"
+    "time"
+    "github.com/kainhuck/cron"
+)
+
+func main() {
+    croner := cron.NewCron()
+    croner.Start(nil)
+
+    croner.AddSecondJob(1, 1, hello(1), cron.ModeJobSerial)
+    croner.AddSecondJob(2, 1, hello(2), cron.ModeTimeFirst)
+
+    time.Sleep(10 * time.Second)
+    croner.RemoveJob(1)
+    croner.RemoveJob(2)
+}
+
+func hello(id int) func() {
+    return func () {
+        fmt.Println("hello", id)
+        time.Sleep(2 * time.Second)
+    }
+}
+
+```
--- cron.go
+++ cron.go
@@ -3,8 +3,9 @@ package cron
 import (
 	"context"
 	"fmt"
-	"github.com/robfig/cron/v3"
 	"sync"
+
+	"github.com/robfig/cron/v3"
 )
 
 // 用于定时同步数据
@@ -12,7 +13,7 @@ import (
 type Cron struct {
 	c      *cron.Cron
 	jobMap sync.Map
-	status uint
+	status sync.Map
 	lock   sync.RWMutex
 }
 
@@ -34,49 +35,58 @@ func NewCron() *Cron {
 	return &Cron{
 		c:      cron.New(cron.WithSeconds()),
 		jobMap: sync.Map{},
-		status: StatusReady,
+		status: sync.Map{},
 		lock:   sync.RWMutex{},
 	}
 }
 
-func (s *Cron) GetStatus() uint {
+func (s *Cron) GetStatus(id int) uint {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
-	return s.status
+	status, ok := s.status.Load(id)
+	if !ok {
+		return StatusReady
+	}
+	return status.(uint)
 }
 
-func (s *Cron) SetStatus(status uint) {
+func (s *Cron) SetStatus(id int, status uint) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	s.status = status
+	s.status.Store(id, status)
 }
 
 // AddJob 添加(更新)任务
-func (s *Cron) AddJob(id int, spec string, f func(), mode RunMode) {
+func (s *Cron) AddJob(id int, spec string, f func(), mode RunMode, immediately ...bool) {
 	var (
 		entryId cron.EntryID
 		err     error
 	)
 
+	var ff func()
+
 	switch mode {
 	case ModeJobSerial:
-		entryId, err = s.c.AddFunc(spec, func() {
-			if s.GetStatus() == StatusRunning {
+		ff = func() {
+			if s.GetStatus(id) == StatusRunning {
 				return
 			}
-			s.SetStatus(StatusRunning)
+			s.SetStatus(id, StatusRunning)
 			f()
-			s.SetStatus(StatusReady)
-		})
-		if err != nil {
-			return
+			s.SetStatus(id, StatusReady)
 		}
 	default:
-		entryId, err = s.c.AddFunc(spec, f)
-		if err != nil {
-			return
-		}
+		ff = f
+	}
+
+	if len(immediately) > 0 {
+		go ff()
+	}
+
+	entryId, err = s.c.AddFunc(spec, ff)
+	if err != nil {
+		return
 	}
 
 	_, ok := s.jobMap.Load(id)
@@ -87,56 +97,56 @@ func (s *Cron) AddJob(id int, spec string, f func(), mode RunMode) {
 }
 
 // AddSecondJob 添加秒级任务
-func (s *Cron) AddSecondJob(id int, sec int, f func(), mode RunMode) {
+func (s *Cron) AddSecondJob(id int, sec int, f func(), mode RunMode, immediately ...bool) {
 	if sec <= 0 || sec >= 60 {
 		return
 	}
 
 	spec := fmt.Sprintf("*/%d * * * * *", sec)
 
-	s.AddJob(id, spec, f, mode)
+	s.AddJob(id, spec, f, mode, immediately...)
 }
 
 // AddMinuteJob 添加分钟任务
-func (s *Cron) AddMinuteJob(id int, min int, f func(), mode RunMode) {
+func (s *Cron) AddMinuteJob(id int, min int, f func(), mode RunMode, immediately ...bool) {
 	if min <= 0 || min >= 60 {
 		return
 	}
 
 	spec := fmt.Sprintf("0 */%d * * * *", min)
 
-	s.AddJob(id, spec, f, mode)
+	s.AddJob(id, spec, f, mode, immediately...)
 }
 
 // AddHourJob 添加小时任务
-func (s *Cron) AddHourJob(id int, hour int, f func(), mode RunMode) {
+func (s *Cron) AddHourJob(id int, hour int, f func(), mode RunMode, immediately ...bool) {
 	if hour <= 0 || hour >= 24 {
 		return
 	}
 
 	spec := fmt.Sprintf("0 0 */%d * * *", hour)
 
-	s.AddJob(id, spec, f, mode)
+	s.AddJob(id, spec, f, mode, immediately...)
 }
 
-func (s *Cron) AddDayJob(id int, day int, f func(), mode RunMode) {
+func (s *Cron) AddDayJob(id int, day int, f func(), mode RunMode, immediately ...bool) {
 	if day <= 0 || day >= 31 {
 		return
 	}
 
 	spec := fmt.Sprintf("0 0 0 */%d * *", day)
 
-	s.AddJob(id, spec, f, mode)
+	s.AddJob(id, spec, f, mode, immediately...)
 }
 
-func (s *Cron) AddMonthJob(id int, mon int, f func(), mode RunMode) {
+func (s *Cron) AddMonthJob(id int, mon int, f func(), mode RunMode, immediately ...bool) {
 	if mon <= 0 || mon >= 12 {
 		return
 	}
 
 	spec := fmt.Sprintf("0 0 0 0 */%d *", mon)
 
-	s.AddJob(id, spec, f, mode)
+	s.AddJob(id, spec, f, mode, immediately...)
 }
 
 // RemoveJob 删除任务
@@ -145,6 +155,7 @@ func (s *Cron) RemoveJob(id int) {
 	if ok {
 		s.c.Remove(eid.(cron.EntryID))
 		s.jobMap.Delete(id)
+		s.status.Delete(id)
 	}
 }
 
